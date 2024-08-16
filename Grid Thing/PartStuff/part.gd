ad3a533a7c4dfafadee93514456a018f9f44dfca
Karanstr <139508165+Karanstr@users.noreply.github.com>
2024-08-16 12:58:48 -0400

    Fixed collision meshing offset from COM
    Also updated to Godot 4.3

--- Grid Thing/PartStuff/part.gd
+++ Grid Thing/PartStuff/part.gd
@@ -1,8 +1,10 @@
 extends RigidBody2D
 
-var grid:Grid = Grid.create(Vector2i(8,8), Vector2(30,30), 2);
+var grid:Grid = Grid.create(Vector2i(8,8), Vector2(10,10), 2);
+@export var com = grid.com;
 
 func _ready():
+	com = grid.com
 	for i in range(grid.dimensions.x):
 		if (i != 1):
 			grid.assign(i, 1)
@@ -11,8 +13,12 @@ func _ready():
 	pass
 
 func _process(_delta):
+	if (grid.com != com):
+		queue_redraw()
+		grid.com = com
+		get_node("CollisionHead").position = -com
 	if Input.is_action_just_pressed("click"):
-		var keys:Array[int] = grid.pointToKey(get_local_mouse_position() + grid.com)
+		var keys:Array[int] = grid.pointToKey(get_local_mouse_position() + com)
 		var key:int = keys[0]
 		if key != -1:
 			var oldVal:int = grid.read(key)
@@ -21,14 +27,17 @@ func _process(_delta):
 			grid.reCacheMeshes([oldVal, newVal]);
 			_updatePhysicsMeshes(1)
 			queue_redraw()
+			#applyBetterForce(Vector2(5,0), Vector2(0, -5))
+			#applyBetterForce(Vector2(-5,0), Vector2(0, 5))
 
-func applyBetterForce(force:Vector2, positionRelativeToCom:Vector2):
-	apply_force(force.rotated(rotation), position + positionRelativeToCom.rotated(rotation))
+
+func applyBetterForce(force:Vector2, offset:Vector2):
+	apply_force(force.rotated(rotation), position + offset.rotated(rotation))
 
 func alignMesh(mesh:Rect2i):
 	var alignedRect:Rect2 = Rect2(mesh)
 	alignedRect.position *= grid.blockLength
-	alignedRect.position -= grid.com
+	#alignedRect.position -= com
 	alignedRect.size *= grid.blockLength
 	return alignedRect
 
@@ -36,6 +45,7 @@ func _updatePhysicsMeshes(blockType:int):
 	get_node("CollisionHead").free()
 	var newHead:Node2D = Node2D.new();
 	newHead.name = "CollisionHead"
+	newHead.position = -com
 	add_child(newHead)
 	var curHead:Node2D = get_node("CollisionHead");
 	for mesh in grid.cachedMeshes[blockType]:
@@ -52,14 +62,14 @@ func _draw():
 			3: color = "Blue"
 		for rect in grid.cachedMeshes[blockMeshes].size():
 			var alignedRect:Rect2 = alignMesh(grid.cachedMeshes[blockMeshes][rect])
+			alignedRect.position -= com
 			draw_rect(alignedRect, color)
-			draw_rect(alignedRect, "black", false, 1)
+	draw_circle(Vector2(0,0), .1, 'blue', true) #Display COM
 	print("Draw complete")
 
 func _makeColBox(rectMesh:Rect2):
-	var colShape = CollisionShape2D.new()
-	colShape.shape = RectangleShape2D.new()
+	var colShape = CollisionShape2D.new();
+	colShape.shape = RectangleShape2D.new();
 	colShape.position = rectMesh.position + rectMesh.size/2;
 	colShape.shape.size = rectMesh.size;
 	return colShape
-
