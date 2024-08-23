7563c49fac0c15c14e18e8858314668f9a282b83
Karanstr <139508165+Karanstr@users.noreply.github.com>
2024-08-23 17:40:52 -0400

    Rendering has been migrated to polygon2D s

--- Grid Thing/RigidGrid/ChunkManager.gd
+++ Grid Thing/RigidGrid/ChunkManager.gd
@@ -5,7 +5,7 @@ var chunkScript = load("res://RigidGrid/chunk_Script.gd")
 func _ready():
 	addNewChunk(Vector2i(0,0))
 
-func addNewChunk(chunkLocation:Vector2i):
+func addNewChunk(_chunkLocation:Vector2i):
 	var newChunk:Node2D = Node2D.new();
 	newChunk.set_script(chunkScript)
 	newChunk.name = "0"
--- Grid Thing/RigidGrid/chunk_Script.gd
+++ Grid Thing/RigidGrid/chunk_Script.gd
@@ -6,6 +6,13 @@ var blockTypes; #Make blockType class and store types in there
 var uniqueBlockData; #NBT Data, Store things like rotation, power level, etc..
 # ^^^^ Maybe move inside of Grid?
 
+func init(gridSize:Vector2, uniqueBlocks:int, gridDimensions:Vector2i = Vector2i(64,64)):
+	grid = Grid.create(gridSize, uniqueBlocks, gridDimensions)
+	grid.reCacheMeshes([0]); #Initial meshing for initial value
+	#_addPhysicsMeshes() #Initial value probably doesn't have physics mesh?
+	_addRenderMeshes(0)
+	pass
+
 func _process(_delta):
 	if Input.is_action_just_pressed("click"):
 		var keys:Array[int] = grid.pointToKey(get_local_mouse_position())
@@ -15,65 +22,72 @@ func _process(_delta):
 			var newVal:int = 1 if oldVal == 0 else 0;
 			grid.assign(key, newVal)
 			_updateMeshes([oldVal, newVal])
-			queue_redraw()
 
 func _updateMeshes(changedVals:Array[int]):
 	for change in changedVals:
-		if (change != 0):
-			_removePhysicsMeshes(change)
+		_removeRenderMeshes(change)
+		_removePhysicsMeshes(change)
 	grid.reCacheMeshes(changedVals);
 	for change in changedVals:
-		if (change != 0):
-			_addPhysicsMeshes(change)
+		_addRenderMeshes(change)
+		_addPhysicsMeshes(change)
 
-func _addPhysicsMeshes(blockType:int):
+func _addRenderMeshes(blockType):
+	var color;
+	match blockType: #Eventually reference block datas
+		0:
+			color = "blue"
+		1:
+			color = "yellow"
 	for meshNum in grid.cachedMeshes[blockType].size():
 		var mesh = grid.cachedMeshes[blockType][meshNum]
-		var colBox:CollisionShape2D = _makeColBox(alignMesh(mesh));
-		colBox.name = encodeMeshName(meshNum, blockType)
-		get_node("../../").add_child(colBox)
+		var polygon = _makeRenderPolygon(mesh, color)
+		polygon.name = encodeMeshName(meshNum, blockType)
+		add_child(polygon)
+
+func _removeRenderMeshes(blockType): 
+	for meshNum in grid.cachedMeshes[blockType].size():
+		get_node(encodeMeshName(meshNum, blockType)).free()
+
+func _addPhysicsMeshes(blockType:int):
+	if (blockType != 0): #If blockType has a collision mesh, reference block datas?
+		for meshNum in grid.cachedMeshes[blockType].size():
+			var mesh = grid.cachedMeshes[blockType][meshNum]
+			var colBox:CollisionShape2D = _makeColBox(mesh);
+			colBox.name = encodeMeshName(meshNum, blockType)
+			get_node("../../").add_child(colBox)
 
 func _removePhysicsMeshes(blockType:int):
-	for meshNum in grid.cachedMeshes[blockType].size():
-		get_node("../../" + encodeMeshName(meshNum, blockType)).free()
+	if (blockType != 0): #If blockType has a collision mesh, reference block datas?
+		for meshNum in grid.cachedMeshes[blockType].size():
+			get_node("../../" + encodeMeshName(meshNum, blockType)).free()
 
 func encodeMeshName(meshNum, blockType):
+	#Chunk Name, followed by encoded mesh name
+	#Chunk Name only matters with collision boxes, but I don't care enough to remove it from the render polygons
 	return name + " " + String.num_int64((meshNum << grid.blocks.packSize) + blockType)
 
-func _makeColBox(rectMesh:Rect2):
+func _makeRenderPolygon(mesh, color):
+	var rect = _meshToLocalRect(mesh);
+	var polygon = Polygon2D.new();
+	var data:PackedVector2Array = PackedVector2Array();
+	data.push_back(rect.position);
+	data.push_back(Vector2(rect.position.x, rect.position.y + rect.size.y));
+	data.push_back(rect.position + rect.size);
+	data.push_back(Vector2(rect.position.x + rect.size.x, rect.position.y));
+	polygon.color = color;
+	polygon.polygon = data;
+	return polygon
+
+func _makeColBox(mesh:Rect2i):
+	var rectMesh = _meshToLocalRect(mesh);
 	var colShape = CollisionShape2D.new();
 	colShape.shape = RectangleShape2D.new();
 	colShape.position = rectMesh.position + rectMesh.size/2;
 	colShape.shape.size = rectMesh.size;
 	return colShape
 
-func init(gridSize:Vector2, uniqueBlocks:int, gridDimensions:Vector2i = Vector2i(64,64)):
-	grid = Grid.create(gridSize, uniqueBlocks, gridDimensions)
-	grid.reCacheMeshes([0]); #Initial meshing for initial value
-	#_addPhysicsMeshes() #Initial value probably doesn't have physics mesh?
-	_addRenderMeshes(0)
-	pass
-
-func _addRenderMeshes(blockType):
-	pass
-
-#Get rid of _draw calls and instead use polygons 2d
-#This will prevent the entire chunk from being recached whenever one blocktype
-#Needs recaching
-func _draw():
-	for blockMeshes in grid.cachedMeshes.size():
-		if (blockMeshes != 0):
-			var color:Color;
-			match blockMeshes:
-				1: color = "Yellow"
-			for rect in grid.cachedMeshes[blockMeshes].size():
-				var alignedRect:Rect2 = alignMesh(grid.cachedMeshes[blockMeshes][rect])
-				draw_rect(alignedRect, color)
-				draw_rect(alignedRect, "black", false, grid.blockLength.x/10)
-	#draw_rect(Rect2(Vector2(0,0), grid.length/50), 'blue', true) #Display COM
-	print("Draw complete")
-
-func alignMesh(mesh:Rect2i):
+func _meshToLocalRect(mesh:Rect2i):
 	var alignedRect:Rect2 = Rect2(mesh)
 	alignedRect.position *= grid.blockLength
 	alignedRect.size *= grid.blockLength
