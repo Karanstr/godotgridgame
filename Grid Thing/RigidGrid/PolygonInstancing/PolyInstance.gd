44dfc6a6778423e2d98336edb82fa369f94cbcd7
Karanstr <139508165+Karanstr@users.noreply.github.com>
2024-08-30 14:01:04 -0400

    I really hate textures (and Godot

--- Grid Thing/RigidGrid/PolygonInstancing/PolyInstance.gd
+++ Grid Thing/RigidGrid/PolygonInstancing/PolyInstance.gd
@@ -1,5 +1,15 @@
 extends Polygon2D
 
+func _ready():
+	setUp(
+		PackedVector2Array([Vector2(0,0), Vector2(8,0), Vector2(8,8), Vector2(0,8)]),
+		"res://RigidGrid/PolygonInstancing/Colors/green.png",
+		Vector2(1, 1)
+		)
+
 #Add function to set up all the stuff
-func setUp(polygonShape:PackedVector2Array, texturePos:Vector2i):
+func setUp(polygonShape:PackedVector2Array, textureName:String, blockSize:Vector2):
+	polygon = polygonShape;
+	texture = load(textureName) #Stupid way to do it, be better pls
+	scale = blockSize
 	pass
