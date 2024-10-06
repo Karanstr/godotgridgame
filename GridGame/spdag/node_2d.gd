a3902fe102f10f4d567884080076433ca803b94d
Ezra Blasko <reynacalterion@gmail.com>
2024-10-05 23:16:33 -0400

    Fixed a lot of insertion errors, still have to figure out the reference being overcounted

--- GridGame/spdag/node_2d.gd
+++ GridGame/spdag/node_2d.gd
@@ -4,12 +4,12 @@ var tree:SparseDimensionalDAG
 
 func _ready():
 	tree = SparseDimensionalDAG.new(3)
-	tree.addData(0b000)
-	tree.addData(0b001)
-	tree.addData(0b010)
-	tree.addData(0b011)
-	#tree.addData(0b100)
-	#tree.addData(0b101)
-	#tree.addData(0b110)
-	#tree.addData(0b111)
+	#tree.addData(0b000)
+	#tree.addData(0b001)
+	#tree.addData(0b010)
+	#tree.addData(0b011)
+	tree.addData(0b100)
+	tree.addData(0b101)
+	tree.addData(0b110)
+	tree.addData(0b111)
 	
--- GridGame/spdag/sdDAG.gd
+++ GridGame/spdag/sdDAG.gd
@@ -3,10 +3,12 @@ class_name SparseDimensionalDAG
 #For now we're going to use arrays and o(n) it instead of hashing/lookups.
 class Nodes:
 	var pot:Array = []
+	var mayNeedRemoving:Array[Dictionary] = []
 	
 	func _init(layers:int):
 		for layer in layers:
 			pot.push_back([])
+			mayNeedRemoving.push_back({})
 		addNode(layers - 1, Branch.new())
 	
 	#This feels like it should be reworked at some point, doesn't matter yet tho
@@ -33,14 +35,38 @@ class Nodes:
 		pot[layer][index] = node
 		return index
 	
-	func removeIfNeeded(layer, index):
-		if pot[layer][index].refCount < 1:
-			pot[layer][index] = -1
+	func removeUneededNodes():
+		for layer in mayNeedRemoving.size():
+			for node in mayNeedRemoving[layer]:
+				if pot[layer][node].refCount < 1:
+					pot[layer][node] = -1 #Eliminate node
+				mayNeedRemoving[layer].clear()
 	
-	func modifyRefCount(layer, index, deltaRef):
-		print("Layer " + String.num(layer) + " Index " + String.num(index))
-		pot[layer][index].refCount += deltaRef
-		removeIfNeeded(layer, index)
+	func removeRef(layer, index):
+		pot[layer][index].refCount -= 1
+		if pot[layer][index].refCount < 1:
+			mayNeedRemoving[layer][index] = true
+			if layer != 0:
+				var setKids:int = pot[layer][index].mask
+				var children:Array[int] = pot[layer][index].children
+				var curIndex:int = 0;
+				while setKids != 0:
+					if setKids & 0b1:
+						removeRef(layer - 1, children[curIndex]) #I hate recursion
+					setKids >>= 1
+					curIndex += 1
+	
+	#/\ && \/ These are very similar, but not the same.. 
+	func addChildRefs(layer, index):
+		if layer != 0:
+			var setKids:int = pot[layer][index].mask
+			var children:Array[int] = pot[layer][index].children
+			var curIndex:int = 0;
+			while setKids != 0:
+				if setKids & 0b1:
+					pot[layer - 1][children[curIndex]].refCount += 1
+				setKids >>= 1
+				curIndex += 1
 
 class Branch:
 	var children:Array[int]
@@ -71,8 +97,8 @@ func getPathIndi(path) -> Array[int]:
 	var curNode = nodes.pot[topLayer][0]
 	var curLayer = topLayer
 	while curLayer != 0: #Descend until we're at the bottom
-		var kidDirection = path >> curLayer
-		if (curNode.mask >> kidDirection) & 1 == 0: #The path ends
+		var kidDirection = (path >> curLayer) & 0b1
+		if (curNode.mask >> kidDirection) & 0b1 == 0: #The path ends
 			return trail
 		var kidIndex = curNode.children[kidDirection & 0b1]
 		trail[curLayer - 1] = kidIndex
@@ -80,6 +106,7 @@ func getPathIndi(path) -> Array[int]:
 		curLayer -= 1
 	return trail
 
+#Refcount isn't quite working yet
 func addData(path:int):
 	var pathIndexes = getPathIndi(path) #Path from leaf[0] to root[size-1]
 	if pathIndexes[0] != -1 && nodes.pot[0][pathIndexes[0]].children[path & 0b1] == 1: 
@@ -88,27 +115,28 @@ func addData(path:int):
 	for layer in pathIndexes.size():
 		var curIndex:int = pathIndexes[layer]
 		var curNode:Branch
-		if curIndex == -1:
+		if curIndex == -1: #Node needs to be made
 			curNode = Branch.new()
 		else: #Node already exists and is referenced in our path
-			curNode = nodes.pot[layer][curIndex].duplicate()
-			nodes.modifyRefCount(layer, curIndex, -1)
+			if layer == topLayer: #Don't mess with the root :(
+				curNode = nodes.pot[layer][curIndex]
+			else:
+				curNode = nodes.pot[layer][curIndex].duplicate()
+				nodes.removeRef(layer, curIndex)
 		var kidDirection = (path >> layer) & 0b1
 		curNode.children[kidDirection] = lastIndex
 		curNode.mask |= 1 << kidDirection
 		curNode.blockCount += 1
 		curIndex = nodes.addNode(layer, curNode)
-		if layer != 0: #Our leaf node technically doesn't exist on layer 0
-			nodes.modifyRefCount(layer - 1, lastIndex, 1)
+		nodes.addChildRefs(layer, curIndex)
 		lastIndex = curIndex
+	nodes.removeUneededNodes()
 
 func readLeaf(path:int):
 	var pathIndexes = getPathIndi(path) #Path from leaf to root
 	if pathIndexes[0] == -1:
 		return 0
 	return nodes.pot[0][pathIndexes[0]].children[path & 0b1]
-
-
 
 func expandTree(_filledQuadrant:int):
 	pass
